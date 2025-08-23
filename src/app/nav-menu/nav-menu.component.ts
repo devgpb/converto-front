166a2c83c43cce94187c571915f1008fc3cf4686
devgpb <bezerra.pereira.dev@gmail.com>
2025-08-22 22:11:33 -0300

    Melhorias II

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -36,15 +36,6 @@ export class NavMenuComponent {
       ],
     },
     {
-      title: 'Sistema',
-      items: [
-        { icon: 'people', title: 'Pedidos', hasSubmenu: true },
-        { icon: 'construct', title: 'Automações', hasSubmenu: true },
-        { icon: 'bar-chart', title: 'Relatório', href: '/relatorio' },
-        { icon: 'server', title: 'Equipamentos', href: '/equipamentos' },
-      ],
-    },
-    {
       title: 'Administração',
       items: [
         { icon: 'people', title: 'Usuários', href: '/ref' },
@@ -55,32 +46,17 @@ export class NavMenuComponent {
     },
   ];
 
-  private submenuClientesRecentItems: SubItem[] = [
-    { icon: 'cash', title: 'Pagamentos Faltantes', route: '/pedidos/detalhes/pagamentos' },
-    { icon: 'cube', title: 'Equipamentos A Comprar', route: '/pedidos/detalhes/equipamentos' },
-    { icon: 'pencil', title: 'ART Faltantes', route: '/pedidos/detalhes/arts' },
-    { icon: 'folder-open', title: 'Homologações Pendentes', route: '/pedidos/detalhes/homologacoes' },
-    { icon: 'cog', title: 'Aguardando Instalação', route: '/pedidos/detalhes/instalar' },
-  ];
 
-  private submenuClientesTableItems: SubItem[] = [
-    { icon: 'document', title: 'Fechado', route: '/pedidos/status/fechado' },
-    { icon: 'cog', title: 'Instalação', route: '/pedidos/status/instalacao' },
-    { icon: 'document', title: 'Nota', route: '/pedidos/status/nota' },
-    { icon: 'document', title: 'Finalizado', route: '/pedidos/status/finalizado' },
-  ];
 
-  private submenuAutItems: SubItem[] = [
-    { icon: 'document', title: 'Gerar Proposta', route: '/proposta/gerar' },
-    { icon: 'document', title: 'Gerar Procuração', route: '/procuracao/gerar' },
-  ];
+  // private submenuAutItems: SubItem[] = [
+  //   { icon: 'document', title: 'Gerar Proposta', route: '/proposta/gerar' },
+  //   { icon: 'document', title: 'Gerar Procuração', route: '/procuracao/gerar' },
+  // ];
 
   public getSubItems(title: string): SubItem[] {
     switch (title) {
-      case 'Pedidos':
-        return [...this.submenuClientesRecentItems, ...this.submenuClientesTableItems];
-      case 'Automações':
-        return this.submenuAutItems;
+      // case 'Automações':
+      //   return this.submenuAutItems;
       default:
         return [];
     }
